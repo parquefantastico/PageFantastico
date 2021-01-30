d0e8e41720149ddc0d86baebe7b7e3f474eb7b6c
parquefantastico <parquefantastico@gmail.com>
2021-01-30 11:25:35 -0600

    add update

---
+++ login/admin/pacific/ListadeContactos.php
@@ -0,0 +1,165 @@
+
+
+<?php include ('conexion.php') ?>
+<?php include ('MenuyHeaderP.php') ?>
+
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+	
+<!-- Optional: include a polyfill for ES6 Promises for IE11 and Android browser -->
+<script src="https://cdn.jsdelivr.net/npm/promise-polyfill"></script>
+<script src="sweetalert2.min.js"></script>
+<link rel="stylesheet" href="sweetalert2.min.css">
+
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        <section class="content-header">
+
+  </section>
+<div class="contenedor">
+ <center>
+      <h1>
+        Lista de contactos
+      </h1>
+  </center><br> 
+ 
+<h4>
+  <div class="row">
+    <div id="cuadro1" class="col-sm-12 col-md-12 col-lg-12">
+      <div class="col-sm-offset-2 col-sm-8">
+        <h3 class="text-center"> <small class="mensaje"></small></h3>
+      </div>
+      <div class="table-responsive col-sm-12">    
+        <table id="dt_pyt" class="table table-bordered table-hover" cellspacing="0" width="100%">
+          <thead>
+            <tr>   
+              <th>Nombre/s</th>
+            <th>Apellido</th>
+            <th>Telefono</th>
+            <th>Correo</th>
+            <th>Campus</th>
+            <th>Carrera</th>
+            <th>Revalidacion</th>
+            <th>Fecha de Nacimiento</th>
+            <th>Fecha de Registro</th>
+            <th>Inscrito</th>
+            <th></th>
+              <th ><a href="index.php" class="btn btn-primary">Formulario</a> </th>                    
+            </tr>
+          </thead>          
+        </table>
+      </div>      
+    </div>    
+  </div>
+  
+
+
+
+      </h1>
+    </section>
+
+    <!-- Main content -->
+    <section class="content container-fluid">
+
+      <!--------------------------
+        | Your Page Content Here |
+        -------------------------->
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+ <?php include ('scripts.php') ?>
+
+  <script>    
+    $(document).on("ready", function(){
+    listar();
+    });
+
+    var listar = function(){
+
+      var table = $("#dt_pyt").DataTable({
+          "ajax":{
+             "method":"POST",
+             "url":"consultaRcontactos.php"
+
+          },
+          "columns":[
+                  
+                 {"data":"NOMBRE"},
+                  {"data":"APELLIDO"},
+                  {"data":"TELEFONO"},
+                  {"data":"CORREO"},
+                  {"data":"CAMPUS"},
+                  {"data":"INTERES"},
+                  {"data":"REVAL"},
+                  {"data":"FECHANAC"},
+                  {"data":"FECHAREG"},
+                  {"data":"REGISTRO"},
+                  {render: function (data, type, row){return `<a class="btn btn-primary" href="index.php?id=${row.ID}" role="button">Editar Contacto</a>`}}
+                  
+
+          ],
+          "language": idioma_espanol
+      });
+
+     
+    }
+
+    var idioma_espanol = {
+      "sProcessing":     "Procesando...",
+    "sLengthMenu":     "Mostrar _MENU_ registros",
+    "sZeroRecords":    "No se encontraron resultados",
+    "sEmptyTable":     "Ningún dato disponible en esta tabla",
+    "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+    "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+    "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+    "sInfoPostFix":    "",
+    "sSearch":         "Buscar:",
+    "sUrl":            "",
+    "sInfoThousands":  ",",
+    "sLoadingRecords": "Cargando...",
+    "oPaginate": {
+        "sFirst":    "Primero",
+        "sLast":     "Último",
+        "sNext":     "Siguiente",
+        "sPrevious": "Anterior"
+    },
+    "oAria": {
+        "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+    }
+    }
+    
+
+  </script>
+  <script type="text/javascript">
+var contr="<?php echo $_SESSION['contrasenasup']?>";
+</script>
+<script type="application/ecmascript">
+
+      function comprobarcontra ($x){
+            var x = $x;
+          (async function getPassword ($x) {
+const {value: password} = await Swal.fire({
+  title: 'Ingrese Contraseña Maestra',
+  input: 'password',
+  inputPlaceholder: 'Ingrese contraseña',
+  inputAttributes: {
+    maxlength: 12,
+    autocapitalize: 'off',
+    autocorrect: 'off'
+  }
+})
+if (password==contr) {
+location.href='PHP/agregarofondo.php?id=3&t='+x;
+}
+})()
+      }
+  </script>
+<?php include ('../footer.php') ?>
+
---
+++ login/admin/pacific/index.php
@@ -0,0 +1,271 @@
+
+<?php include ('MenuyHeaderP.php') ?>
+<?php include ('../conexion.php')?>
+ <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        <section class="content-header">
+
+
+
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+  
+  <!-- Optional: include a polyfill for ES6 Promises for IE11 and Android browser -->
+  <script src="https://cdn.jsdelivr.net/npm/promise-polyfill"></script>
+  <script src="sweetalert2.min.js"></script>
+  <link rel="stylesheet" href="sweetalert2.min.css">
+  
+  <?php 
+  
+  if(isset($_GET['f'])==true){
+  
+  ?>
+  <script type="text/javascript">
+    const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000
+  });
+  
+  Toast.fire({
+    type: 'success',
+    title: '<h1> Contacto añadido con exito</h1>',
+    width: 600,
+    padding: '1em',
+  })
+  </script>
+  
+  
+  
+  <?php
+  }
+  ?>
+  
+
+
+<script>
+function hideshow(){
+var frm=document.form1;
+if(frm.style.display=="block"){frm.style.display="none"}
+else
+if(frm.style.display=="none"){frm.style.display="block"}
+}
+</script>
+
+  <h1>Nuevo Contacto</h1>
+  <br>
+ 
+  
+      <button onclick="window.location.href='ListadeContactos.php'" class="btn btn-primary">Lista de Contactos</button>
+    
+
+  <section class="content">
+<div class="row">
+  <div class="col-md-12">
+
+<div class="box box-primary"><br>
+    <form class="form-horizontal" method="post" autocomplete="off" id="addproduct" action="agregarContactos.php" role="form">
+       <div class="form-group">
+   
+
+  <div class="form-group">
+    <label for="inputEmail1" class="col-lg-2 control-label">Nombre</label>
+    <div class="col-md-6">
+      <input type="text" required name="nombre" autocomplete="off" autofocus   class="form-control"  class="form-control" id="input1" placeholder="Nombre">
+
+    </div>
+  </div>
+
+
+  <div class="form-group">
+    <label for="inputEmail1" class="col-lg-2 control-label">Apellido</label>
+    <div class="col-md-6">
+      <input type="text" name="apellido" required autocomplete="off"  class="form-control"   class="form-control" id="input2"  placeholder="Apellido">
+
+    </div>
+  </div>
+ <div class="form-group">
+    <label for="inputEmail1" class="col-lg-2 control-label">Telefono</label>
+    <div class="col-md-4">
+      <input type="number" required autocomplete="off" name="tel"  class="form-control"   class="form-control" id="input3"  placeholder="Telefono">
+
+    </div>
+  </div>
+   <div class="form-group">
+    <label for="inputEmail1" class="col-lg-2 control-label">Correo Electronico</label>
+    <div class="col-md-6">
+      <input type="email" required name="correo" autocomplete="off"  class="form-control"   class="form-control" id="input4"  placeholder="Correo">
+
+    </div>
+    
+  </div>
+  <div class="form-group">
+    <label for="inputEmail1" class="col-lg-2 control-label">Fecha de nacimiento</label>
+    <div class="col-md-6">
+      <input type="date" name="fecha_nac" autocomplete="off"  class="form-control"   class="form-control" id="input4"  placeholder="Correo">
+
+    </div>
+    
+  </div>
+
+
+
+   <div class="form-group">
+    <label for="inputEmail1" class="col-lg-2 control-label">Campus</label>
+    <div class="col-md-6">
+     
+      <select name="Campus"  class="form-control"  pattern= "[0-9]+" class="form-control" id="input5"  placeholder="Cantidad">
+  <option value="Coatzacoalcos" autofocus>Coatzacoalcos</option>
+  
+</select>
+ 
+    </div>
+  </div>
+  <div class="form-group">
+               <?php
+
+include 'conexion.php' ;
+$query=mysqli_query($conexion,"SELECT id_materias,nombre_materia FROM materias_pc");
+
+?>
+    <label for="inputEmail1" class="col-lg-2 control-label">Carreras</label>
+    <div class="col-md-6">
+    <select name="carrera" class="form-control" id="lugar"> 
+        <?php
+    while($datos = mysqli_fetch_array($query) )
+    {
+        
+    
+    ?>
+        <option value="<?php echo $datos['id_materias'] ?>"> <?php echo $datos['nombre_materia'] ?> </option>
+        <?php
+    }
+        ?>
+        
+        </select>
+        
+    </div>
+  </div>
+<div class="form-group">
+    <label for="inputEmail1" class="col-lg-2 control-label">revalidacion</label>
+    <div class="col-md-6">
+
+      <select name="revalidacion"  class="form-control" pattern= "[0-9]+" class="form-control" id="input6"  >
+  <option value="NO">NO</option>
+  <option value="SI">SI</option>
+  >
+</select>
+    </div>
+  </div>
+
+  <div class="form-group">
+    <label for="inputEmail1" class="col-lg-2 control-label">¿Aplico a otra U?</label>
+    <div class="col-md-6">
+
+      <select name="uni-unic"     class="form-control" pattern= "[0-9]+" class="form-control" id="input6"  >
+  <option value="NO">NO</option>
+  <option value="SI">SI</option>
+  >
+</select>
+    </div>
+  </div>
+  
+ 
+  
+
+  <div class="form-group">
+    <div class="col-lg-offset-2 col-lg-10">
+      <button type="submit" id="submit" class="btn btn-primary">Agregar Contacto</button>
+    </div>
+  </div>
+
+  </div>
+
+
+
+
+
+
+  </tbody>
+</table>
+</h4>
+  </div>
+
+      </h1>
+
+
+
+
+
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+        <li class="active">Here</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content container-fluid">
+
+      <!--------------------------
+        | Your Page Content Here |
+        -------------------------->
+
+    </section>
+    <!-- /.content -->
+  </div>
+
+<script>
+// Funcion que se ejecuta cada vez que se pulsa una tecla en cualquier input
+// Tiene que recibir el "event" (evento generado) y el siguiente id donde poner
+// el foco. Si ese id es "submit" se envia el formulario
+function saltar(e,id)
+{
+	// Obtenemos la tecla pulsada
+	(e.keyCode)?k=e.keyCode:k=e.which;
+ 
+	// Si la tecla pulsada es direc derecha (codigo ascii 13)
+	if(k==39)
+	{
+		// Si la variable id contiene "submit" enviamos el formulario
+		if(id=="submit")
+		{
+			document.forms[0].submit();
+		}else{
+			// nos posicionamos en el siguiente input
+			document.getElementById(id).focus();
+		}
+	}
+}
+function regresar(e,id)
+{
+	// Obtenemos la tecla pulsada
+	(e.keyCode)?k=e.keyCode:k=e.which;
+ 
+	// Si la tecla pulsada es direc derecha (codigo ascii 13)
+	if(k==37)
+	{
+		// Si la variable id contiene "submit" enviamos el formulario
+		if(id=="submit")
+		{
+			document.forms[0].submit();
+		}else{
+			// nos posicionamos en el siguiente input
+			document.getElementById(id).focus();
+		}
+	}
+}
+
+
+
+
+</script>
+
+
+
+
+  
+  
+<?php include ('../footer.php') ?>
+<?php include ('../scripts.php') ?>
